98d639f4e2c708dfb4586c4541c686c049e7be51
Eddie Moon <eddiemoonie@gmail.com>
2018-05-29 21:27:15 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    self.order(highest_rating).first
+    self.where(highest_rating).first
   end
 
   def self.lowest_rating
@@ -13,7 +13,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    self.where(lowest_rating(:rating)).first
+    self.order(lowest_rating).first
   end
 
   def self.ratings_sum
